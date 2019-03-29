bb0ed52030f673e973611db35aaf2f04005aa31d
yjg <ccpitcmp@outlook.com>
2019-03-29 16:37:07 +0800

    add ip addr in aop log test

--- src/main/java/com/jungle/app/aop/logging/LoggingAspect.java
+++ src/main/java/com/jungle/app/aop/logging/LoggingAspect.java
@@ -2,8 +2,10 @@ package com.jungle.app.aop.logging;
 
 import io.github.jhipster.config.JHipsterConstants;
 
+import javassist.bytecode.SignatureAttribute;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,8 +13,15 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Aspect for logging execution of service and repository Spring components.
@@ -94,5 +103,82 @@ public class LoggingAspect {
 
             throw e;
         }
+    }
+
+    /**
+     * add ip addr aop log test 20190329
+     * @return
+     * @throws Throwable
+     */
+    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        long beginTime = System.currentTimeMillis();//1、开始时间
+        //利用RequestContextHolder获取requst对象
+        ServletRequestAttributes requestAttr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        String uri = requestAttr.getRequest().getRequestURI();
+        log.info("start timing : {}  URI: {}", new Date(),uri);
+        //访问目标方法的参数 可动态改变参数值
+        Object[] args = joinPoint.getArgs();
+        //方法名获取
+        String methodName = joinPoint.getSignature().getName();
+        log.info("request method：{}, request parameters: {}", methodName, Arrays.toString(args));
+        //可能在反向代理请求进来时，获取的IP存在不正确行 这里直接摘抄一段来自网上获取ip的代码
+        log.info("request ip：{}", getIpAddr(requestAttr.getRequest()));
+
+/*        Signature signature = joinPoint.getSignature();
+        if(!(signature instanceof SignatureAttribute.MethodSignature)) {
+            throw new IllegalArgumentException("暂不支持非方法注解");
+        }*/
+        //调用实际方法
+        Object object = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        log.info("end timing: {},  URI: {},cost：{}", new Date(),uri,endTime - beginTime);
+        //模拟异常
+        //System.out.println(1/0);
+        return object;
+    }
+
+    /**
+     * aop log test 20190329
+     * 转至：https://my.oschina.net/u/994081/blog/185982
+     */
+    public String getIpAddr(HttpServletRequest request) {
+        String ipAddress = null;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                log.info("get ip getRemoteAddr：{}" ,ipAddress);
+                if (ipAddress.equals("127.0.0.1")) {
+                    // 根据网卡取本机配置的IP
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        log.error("get ip error：{}" ,e.getMessage());
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+        // ipAddress = this.getRequest().getRemoteAddr();
+
+        return ipAddress;
     }
 }
